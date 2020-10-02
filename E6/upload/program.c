64f4c9216590daa6ec34053e6df257a89127bde8
unknown <321amalpb.com@gmail.com>
2020-10-02 14:29:37 +0530

    Changed experiment 6

--- E6/upload/program.c
+++ E6/upload/program.c
@@ -1,133 +1,99 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdlib.h>
-FILE *fout;
-void *producer (void *) ;
-void *consumer(void *) ;
-sem_t empty, full, mutex ;
-int prate[20], crate[20],n,pcount = 0,ccount = 0 ;
 
-int buffer[10] /*note extra long space!*/ ;
-int ID[10] ;
-int BUFFER_SIZE = 20 ;
-int n;
-int store=0;
+#include<stdio.h>
+#include<pthread.h>  	//library for multi threading
+#include<semaphore.h>   //library to implement semaphores
+#include<unistd.h>	//library for sleep() function
 
-void main() {
-    pthread_t TID[10] ;
-    FILE *fin;
-    int i,j,count=0;
-    fin= fopen("input.txt","r");
-    fout = fopen("output.txt", "w");
-    if ( fin == NULL ) 
-    { 
-        printf( "input.txt file failed to open." ) ; 
-    } 
-    else{
-        fscanf (fin, "%d %d ", &i, &j);    
-        while (!feof (fin))
-        {   
-            prate[count] = i;
-            crate[count] = j;
+//size is the total buffer size and buffer is the current allocation of buffer
+int size=0,buffer=0;
+sem_t s; 
+FILE *fp;
 
-            // printf ("%d %d %d\n", i,j,k);
-            fscanf (fin, "%d %d", &i, &j); 
-            count++;
-        }
-        prate[count] = i;
-        crate[count] = j;
-    }
-    n = count;
+//structres for production rate and consumption rate data;
+struct prod {
+	int n, rate[10];
+}P;
 
-    printf("\n\n------Output------\n\n");
-    fprintf(fout,"------Output------\n\n");
+struct cons {
+	int n, rate[10];
+}C;
 
-    sem_init(&empty, 0, BUFFER_SIZE) ;
-    sem_init(&full, 0, 0) ;
-    sem_init(&mutex, 0, 1) ;
 
-    for(i = 0; i < 10; i++) {
-        ID[i] = i ;
-        buffer[i] = -1 ;
-    }
-
-    //for(i = 0; i < 5000; i += 2) {
-        pthread_create(&TID[0], NULL, producer, (void *) &ID[0]) ;
-        
-        pthread_create(&TID[1], NULL, consumer, (void *) &ID[1]) ;
-      
-
-       
-    //}
-
-      
-    
-    for(i = 0; i < 1 ; i++) {
-        pthread_join(TID[i], NULL) ;
-    }
-    sem_destroy(&empty);
-    sem_destroy(&full);
-    sem_destroy(&mutex);
-    printf("Buffer space: %d\n",BUFFER_SIZE);
-    fprintf(fout,"Buffer space: %d\n",BUFFER_SIZE);
-   
-    fclose(fout);
-    fclose (fin);  
+//producer function that runs on one thread
+void *producer() {
+	//current array index initialized to 0
+	int current = 0;
+	//loop till entire array items are produced
+	while(current<P.n) {
+		//wait till other function releases lock and then apply lock to begin next iteration
+		sem_wait(&s);
+		if(buffer + P.rate[current] <= size) {
+			//producer can produce
+			buffer += P.rate[current];
+			current ++;
+			printf("Producer produced succesfully\nBuffer Filled : %d\n\n",buffer);
+			fprintf(fp,"Producer produced succesfully\nBuffer Filled : %d\n\n",buffer);
+		}
+		else {
+			//producer cannot produce
+			printf("Producer cannot produce\n\n");
+			fprintf(fp,"Producer cannot produce\n\n");
+		}
+		//release locks
+		sem_post(&s);
+		sleep(2);
+	}
 }
 
-void *producer(void *Boo) {
-    int *ptr;
-    int ID;
-    ptr = (int *) Boo;
-    ID = *ptr;
-    while (1) {
-        
-        sem_wait(&empty);
-        sem_wait(&mutex);
-        
-        if (store+prate[pcount]<BUFFER_SIZE && pcount<=n)
-        {         
 
-            printf("Producer produced %d items\n",prate[pcount] );
-            fprintf(fout,"Producer produced %d items\n",prate[pcount] );
-            store+=prate[pcount];
-            pcount = (pcount + 1);
-            printf("Buffer space: %d\n\n", store);
-            fprintf(fout,"Buffer space: %d\n\n", store);
-        }
+//consumer function that runs on the other thread
+void *consumer() {
+	//current array index initialized to 0
+	int current = 0;
+	//loop till entire array items are consumed
+	while(current<C.n) {
+		//wait till other function releases lock and then apply lock to begin next iteration
+		sem_wait(&s); 
+		if(buffer - C.rate[current] > 0) {
+			//consumer can consume
+			buffer -= C.rate[current];
+			current ++;
+			printf("Consumer consumed succesfully\nBuffer Filled : %d\n\n",buffer);
+			fprintf(fp,"Consumer consumed succesfully\nBuffer Filled : %d\n\n",buffer);
+		}
+		else {
+			//consumer cannot consume
+			printf("Consumer cannot consume\n\n");
+			fprintf(fp,"Consumer cannot consume\n\n");
+		}
+		//release lock
+		sem_post(&s); 
+		sleep(1);
+	}
+}
 
-        sem_post(&mutex);
-        sem_post(&full);
-    }
- }
 
-void *consumer (void *Boo) {
-    static int nextConsumed = 0 ;
-    int *ptr ;
-    int ID ;
-    ptr = (int *) Boo ;
-    ID = *ptr ;
-    while (1) {
-        sem_wait(&full);
-        sem_wait(&mutex);
 
-        if (store-crate[ccount]>0 && ccount<=n)
-        {
-        
-
-            printf("Consumer consumed %d items\n",crate[ccount] );
-            fprintf(fout,"Consumer consumed %d items\n",crate[ccount] );
-
-            store -= crate[ccount];
-            ccount = (ccount+1);    
-            printf("Buffer space: %d\n\n", store);
-            fprintf(fout,"Buffer space: %d\n\n", store);
-        
-        }
-
-        sem_post(&mutex);
-        sem_post(&empty);
-
-    }
+int main() {
+	int i;
+	//initialise semaphore variable
+	sem_init(&s, 0, 1);
+	fp = fopen("output.txt","w");
+	//recieve inputs
+	scanf("%d",&size);
+	scanf("%d",&P.n);
+	for(i=0;i<P.n;i++)
+		scanf("%d",&P.rate[i]);
+	scanf("%d",&C.n);
+	for(i=0;i<C.n;i++)
+		scanf("%d",&C.rate[i]);
+	//declare two thread id's
+	pthread_t ptid,ctid;
+	//pthread_create creates and calls the functions which are to be run in the thread
+	pthread_create(&ptid,NULL,producer,NULL);
+    	pthread_create(&ctid,NULL,consumer,NULL);
+    	//pthread_join makes sure that the main() function will not run till the respecive threads have finished execution
+    	pthread_join(ptid,NULL);
+    	pthread_join(ctid,NULL);
+	return 0;
 }
