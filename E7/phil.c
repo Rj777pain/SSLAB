572d107348f649e143931f6e7b3fa447bbd94ae6
Rj777pain <rejathtm@gmail.com>
2020-10-13 15:59:57 +0530

    Exp 7

---
+++ E7/phil.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include <pthread.h> 
+#include <semaphore.h>
+#define N 7 
+
+sem_t fork[N];
+sem_t max;
+
+void *philosopher(int pno){
+    int i,j;
+    i=pno;
+    j=(i+1)%N;
+    printf("\nPhilosoper %d is Waiting to Eat",i+1);
+    sem_wait(&max);
+    //printf("\nPhilosopher%d is Sitting on Table",i);
+    sem_wait(&fork[i]);
+    sem_wait(&fork[j]);
+    printf("\nPhilosopher %d is Eating",i+1);
+    sem_post(&fork[i]);
+    sem_post(&fork[j]);
+    printf("\nPhilosopher %d Finished Eating",i+1);
+    sem_post(&max);
+    //printf("\nPhilosopher%d Left",i);
+}
+void main(){
+    int a[N];
+    pthread_t phil[N];
+    sem_init(&max,0,N/2);
+    for(int i=0;i<N;i++){
+        sem_init(&fork[i],0,1);
+        a[i]=i;
+    }
+    for(int i=0;i<N;i++)
+        pthread_create(&phil[i],NULL,(void *)philosopher,(void *)a[i]);
+
+    for(int i=0;i<N;i++){
+        pthread_join(phil[i],NULL);
+    }
+
+    sem_destroy(&max);
+    for(int i=0;i<N;i++)
+        sem_destroy(&fork[i]);
+}
